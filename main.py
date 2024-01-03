d0487b643240c0762de9bff6a884c990abe743d8
Rosinsd <152635516+Rosinsd@users.noreply.github.com>
2024-01-03 20:57:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Code calculates your number multiplied  0-10 and shows you on the Console
+#Created by Jayden Smith
+sum = int(input("Enter a positive integer: "))
+i = 0
+while i <= 10:
+   print(i*sum)
+   i=i+1
